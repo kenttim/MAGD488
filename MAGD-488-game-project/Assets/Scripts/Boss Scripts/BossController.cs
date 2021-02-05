76017a30bf41fe9deb2478c9f9e87119e80b8e48
TexTheRobin <67350449+TexTheRobin@users.noreply.github.com>
2021-02-04 21:49:13 -0600

    Nav mesh installation
    Added Nav Mesh Scripts
    created a basic level for testing.
    created a prefab of the Boss and Player

---
+++ MAGD-488-game-project/Assets/Scripts/Boss Scripts/BossController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+
+/*
+    BE SURE TO APPLY A NAV MESH AGENT TO BOTH THE PLAYER AND THE BOSS
+ */
+
+public class BossController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    
+    public NavMeshAgent player;
+
+    public int frequency;
+    private int timer;
+    // Update is called once per frame
+    void Start()
+    {
+        timer = frequency;
+    }
+    void Update()
+    {
+        timer -= 1;
+        if (timer < 0)
+        {
+            agent.SetDestination(GameObject.Find("player").transform.position);
+            timer = frequency;
+        }
+        
+    }
+}
+/*
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController : MonoBehaviour
+{
+
+    public NavMeshAgent boss;
+
+    
+
+    private int timer = 60;
+
+    void Update()
+    {
+        timer -= 1;
+        if (timer <= 0)
+        {
+            
+            timer = 60;
+        }
+    }
+}
+*/
