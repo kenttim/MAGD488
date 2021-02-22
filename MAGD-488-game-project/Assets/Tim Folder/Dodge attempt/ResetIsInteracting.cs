174b6a7c549cba04bcd1d0f77c645f70ce6a0b3c
limitDak <34105863+limitDak@users.noreply.github.com>
2021-02-21 22:54:29 -0600

    Dodge
    Dodge animation added but its janky af

--- MAGD-488-game-project/Assets/Tim Folder/Dodge attempt/ResetIsInteracting.cs
+++ MAGD-488-game-project/Assets/Tim Folder/Dodge attempt/ResetIsInteracting.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResetIsInteracting : MonoBehaviour
+public class ResetIsInteracting : StateMachineBehaviour
 {
     //dodge stuff
-   /* override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.SetBool("isInteracting", false);
-    }
-    */
+     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+     {
+         animator.SetBool("isInteracting", false);
+     }
+     
 }
