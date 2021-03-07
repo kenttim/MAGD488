f6390f56db7de30005ca1dc3ac3a2057eba77cc6
limitDak <34105863+limitDak@users.noreply.github.com>
2021-03-07 16:26:04 -0600

    Dak - Quick Fix
    Fixed Tim's Testing ground w new stuff that I put in

--- MAGD-488-game-project/Assets/Dak_Playground/Scripts/Player_Scripts/PlayerLocomotion.cs
+++ MAGD-488-game-project/Assets/Dak_Playground/Scripts/Player_Scripts/PlayerLocomotion.cs
@@ -211,7 +211,7 @@ public class PlayerLocomotion : MonoBehaviour
                 }
                 else
                 {
-                    animatorHandler.PlayTargetAnimation("Locomotion", false);
+                    animatorHandler.PlayTargetAnimation("Empty", false);
                     inAirTimer = 0;
                 }
                 playerManager.isInAir = false;
@@ -235,7 +235,15 @@ public class PlayerLocomotion : MonoBehaviour
                 rigidbody.velocity = vel * (movementSpeed / 2);
                 playerManager.isInAir = true;
             }
-        }
+
+            if(inputHandler.isInteracting || inputHandler.moveAmount > 0)
+            {
+                myTransform.position = Vector3.Lerp(myTransform.position, targetPosition, Time.deltaTime / 0.1f);
+            } else
+            {
+                myTransform.position = targetPosition;
+            }
+        } 
 
         if (playerManager.isGrounded)
         {
