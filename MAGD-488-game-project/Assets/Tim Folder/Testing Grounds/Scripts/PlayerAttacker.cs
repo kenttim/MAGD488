6b205530e2de6ee237816369f0d0f6d7ebcbf92a
Tim Kent <pooth46@yahoo.com>
2021-03-10 15:35:55 -0600

    errors and bugs fixed and moved some scripts into my script folder

--- MAGD-488-game-project/Assets/Tim Folder/Testing Grounds/Scripts/PlayerAttacker.cs
+++ MAGD-488-game-project/Assets/Tim Folder/Testing Grounds/Scripts/PlayerAttacker.cs
@@ -6,21 +6,33 @@ public class PlayerAttacker : MonoBehaviour
 {
     AnimatorHandler animatorHandler;
     WeaponSlotManager weaponSlotManager;
+    PlayerManager playerManager;
 
     private void Awake()
     {
         animatorHandler = GetComponentInChildren<AnimatorHandler>();
         weaponSlotManager = GetComponentInChildren<WeaponSlotManager>();
+        playerManager = GetComponent<PlayerManager>();
     }
 
     public void HandleLightMeleeAttack(WeaponItem weapon)
     {
+        if (playerManager.isInteracting)
+        {
+            return;
+        }
+
         weaponSlotManager.attackingWeapon = weapon;
         animatorHandler.PlayTargetAnimation(weapon.L_Attack_1, true);
     }
 
     public void HandleHeavyMeleeAttack(WeaponItem weapon)
     {
+        if (playerManager.isInteracting)
+        {
+            return;
+        }
+
         weaponSlotManager.attackingWeapon = weapon;
         animatorHandler.PlayTargetAnimation(weapon.H_Attack_1, true);
     }
