39fa630c93de2fcd52b90faa6421005ae678460f
limitDak <34105863+limitDak@users.noreply.github.com>
2021-03-25 13:09:21 -0500

    Enemy AI Revamp
    What's new?
    - Base classes for animation and stats
    -Enemy AI to follow and detect player
    -Enemy animation that works like player animation (rn)

--- MAGD-488-game-project/Assets/Dak_Playground/Scripts/Enemy/Enemy_Manager.cs
+++ MAGD-488-game-project/Assets/Dak_Playground/Scripts/Enemy/Enemy_Manager.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class Enemy_Manager : MonoBehaviour
 {
+
+    EnemyLocomotionManager enemyLocomotionManager;
+
+    public bool isPerformingAction;
+
+    [Header("A.I. Settings")]
+    public float detectionRadius = 20;
+    //the higher the maximum and lower the minimum the greater the fov becomes
+    public float maximumDetectionAngle = 50;
+    public float minimumDetectionAngle = -50;
+
+
+    private void Awake()
+    {
+        enemyLocomotionManager = GetComponent<EnemyLocomotionManager>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        HandleCurrentAction();
+    }
+
+    private void HandleCurrentAction()
+    {
+        if (enemyLocomotionManager.currentTarget == null)
+        {
+            enemyLocomotionManager.HandleDetection();
+        } else
+        {
+            enemyLocomotionManager.HandleMoveToTarget();
+        }
+    }
+
     
 }
